3db7672c08a6b80ec425cedb9f942419d41a7882
user-PC\user <repinsky>
2019-02-12 01:25:47 +1000

    carousel

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
-	//...
-})
+$(document).ready(function () {
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items: 5,
+        loop: true,
+        margin: 100,
+        // autoWidth: true,
+        autoplay: true,
+        slideTransition: 'linear',
+        autoplayTimeout: 0,
+        autoplaySpeed: 10000,
+        autoplayHoverPause: false
+    });
+
+    $('.item').each(function(){
+        $(this).css('animation-delay', Math.random() * (2) + 1 + 's')
+    });
+});
